a788db34681e7a452a25e6f98e6e57d47baed824
Reef8 <sharifk8@gmail.com>
2026-01-05 12:30:49 -0800

    Remove unused phoenix-colocated import that breaks prod build

--- assets/js/app.js
+++ assets/js/app.js
@@ -22,7 +22,6 @@ import "phoenix_html"
 // Establish Phoenix Socket and LiveView configuration.
 import {Socket} from "phoenix"
 import {LiveSocket} from "phoenix_live_view"
-import {hooks as colocatedHooks} from "phoenix-colocated/bracket_battle"
 import topbar from "../vendor/topbar"
 
 // Custom hooks
@@ -267,7 +266,7 @@ const csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute
 const liveSocket = new LiveSocket("/live", Socket, {
   longPollFallbackMs: 2500,
   params: {_csrf_token: csrfToken},
-  hooks: {...colocatedHooks, ...Hooks},
+  hooks: Hooks,
 })
 
 // Show progress bar on live navigation and form submits
